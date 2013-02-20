34679800ca7930e511871b5dc57ab42305a35de5
Edmund Qiu <edmundjqiu@gmail.com>
2013-02-20 15:18:03 -0500

    Added a lot of files pertaining to the scripting engine.

--- SpaceInvaders/src/spaceinvaders/LevelManager.java
+++ SpaceInvaders/src/spaceinvaders/LevelManager.java
@@ -9,14 +9,37 @@ package spaceinvaders;
  * @author Edmund
  */
 public class LevelManager implements Scriptable {
+    private int scriptID;
+    private int currentLineNumber;
+    private boolean inProgress;
+    
+    public LevelManager()
+    {
+        scriptID = 1;
+        currentLineNumber = 0;
+        inProgress = false;
+    }
+    
     
     public void spawnEntity() {
         
     
     }
     
+    public int getLineNumber()
+    {
+        return currentLineNumber;
+    }
     
+    public int getScriptID()
+    {
+        return scriptID;
+    }
     
+    public boolean isRunning()
+    {
+        return inProgress;
+    }
     
-    
+  
 }
