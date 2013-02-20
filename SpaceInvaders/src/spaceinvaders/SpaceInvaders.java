6ac308023eac3427e697323b79e417dc5ffff231
Brian Yang <brian@brianyang.me>
2013-02-19 22:37:37 -0500

    Add a short description, remove the print, and add a version number

--- SpaceInvaders/src/spaceinvaders/SpaceInvaders.java
+++ SpaceInvaders/src/spaceinvaders/SpaceInvaders.java
@@ -6,8 +6,11 @@ package spaceinvaders;
 
 /**
  * Space Invaders Game
+ * A simple space invaders game
+ * 
  * @author Brian Yang
  * @author Edmund Qiu
+ * @version 0.1
  */
 public class SpaceInvaders {
 
@@ -15,7 +18,6 @@ public class SpaceInvaders {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.print("Hello world!");
+        
     }
 }
