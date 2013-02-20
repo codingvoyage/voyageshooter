27ceb181e8b396e221fd207d169ea4f6da0d91f3
Brian Yang <brian@brianyang.me>
2013-02-19 22:48:36 -0500

    Very simple "Hello World" using the test code provided by Slick2D's Wiki.

--- SpaceInvaders/src/spaceinvaders/SpaceInvaders.java
+++ SpaceInvaders/src/spaceinvaders/SpaceInvaders.java
@@ -4,6 +4,8 @@
  */
 package spaceinvaders;
 
+import org.newdawn.slick.*;
+
 /**
  * Space Invaders Game
  * A simple space invaders game
@@ -12,12 +14,29 @@ package spaceinvaders;
  * @author Edmund Qiu
  * @version 0.1
  */
-public class SpaceInvaders {
+public class SpaceInvaders extends BasicGame {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        
+    public SpaceInvaders() {
+       super("Hello World");
+    }
+
+    public void init(GameContainer gc) throws SlickException {
+
+    }
+
+    public void update(GameContainer gc, int delta) throws SlickException {
+
+    }
+
+    public void render(GameContainer gc, Graphics g) throws SlickException {
+       g.drawString("Hello World", 100, 100);
+    }
+
+    public static void main(String[] args) throws SlickException
+    {
+       AppGameContainer app = new AppGameContainer(new SpaceInvaders());
+
+       app.setDisplayMode(800, 600, false);
+       app.start();
     }
 }
