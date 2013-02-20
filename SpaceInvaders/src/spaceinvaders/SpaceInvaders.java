3d466771a5ed3488d9700459fa89831ade29a024
Brian Yang <brian@brianyang.me>
2013-02-20 01:12:55 -0500

    Update SpaceInvaders/src/spaceinvaders/SpaceInvaders.java

--- SpaceInvaders/src/spaceinvaders/SpaceInvaders.java
+++ SpaceInvaders/src/spaceinvaders/SpaceInvaders.java
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-=======
-
->>>>>>> origin/BrianBranch
 package spaceinvaders;
 
 import org.newdawn.slick.*;
@@ -18,18 +10,11 @@ import org.newdawn.slick.*;
  * @author Edmund Qiu
  * @version 0.1
  */
-<<<<<<< HEAD
-public class SpaceInvaders extends BasicGame {
-
-    public SpaceInvaders() {
-       super("Hello World");
-=======
 
 public class SpaceInvaders extends BasicGame {
 
     public SpaceInvaders() {
        super("We are Team Coding Voyage!");
->>>>>>> origin/BrianBranch
     }
 
     public void init(GameContainer gc) throws SlickException {
@@ -44,22 +29,14 @@ public class SpaceInvaders extends BasicGame {
         //Any and all graphics/rendering functions which should be called
         //with the drawing of each frame go HERE
         
-<<<<<<< HEAD
-        g.drawString("Hello World", 100, 100);
-=======
         g.drawString("We are Team Coding Voyage!", 100, 100);
->>>>>>> origin/BrianBranch
     }
 
     public static void main(String[] args) throws SlickException
     {
        AppGameContainer app = new AppGameContainer(new SpaceInvaders());
-
-<<<<<<< HEAD
-       app.setDisplayMode(800, 600, false);
-=======
+       
        app.setDisplayMode(1024, 768, false);
->>>>>>> origin/BrianBranch
        app.start();
     }
 }
