139ff3bd4e5ab18a263a9e19f4e094e4d1edbcc0
Brian Yang <me@brianyang.me>
2013-02-19 22:23:01 -0500

    Adding the main class

---
+++ SpaceInvaders/src/spaceinvaders/SpaceInvaders.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spaceinvaders;
+
+/**
+ * Space Invaders Game
+ * @author Brian Yang
+ * @author Edmund Qiu
+ */
+public class SpaceInvaders {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Hello world!");
+    }
+}
