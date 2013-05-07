b84b4ba59eb2b7f8a271ccf14fc3028a149f1973
Brian Yang <brian@brianyang.me>
2013-05-07 19:55:49 -0400

    Initial non-repo setup commit - Slick2D, LWJGL, and GSON libraries - Slick2D source code for code autocomplete and Javadoc - Main methods in VoyageQuest.java setup - Initial Gui.java file

---
+++ VoyageQuest/src/gui/Gui.java
@@ -0,0 +1,13 @@
+package gui;
+
+/**
+ * Voyage GUI is a custom designed graphical user interface 
+ * intended to handle display of items such as the player's 
+ * inventory, quest manager, dialog boxes, and more.
+ * 
+ * @author Brian Yang
+ * @version 1.0
+ */
+public abstract class Gui<E> {
+    
+}
---
+++ VoyageQuest/src/voyagequest/VoyageQuest.java
@@ -0,0 +1,83 @@
+package voyagequest;
+
+import org.newdawn.slick.*;
+// import scripting.*;
+// import gui.*;
+
+/**
+ * Voyage Quest RPG
+ * Copyright (c) 2013 Team Coding Voyage.
+ * 
+ * @author Edmund Qiu
+ * @author Brian Yang
+ */
+
+public class VoyageQuest extends BasicGame {
+    
+    /** x resolution */
+    public static int X_RESOLUTION = 1024;
+    /** y resolution */
+    public static int Y_RESOLUTION = 768;
+    /** full screen mode */
+    public static boolean FULLSCREEN = false;
+    
+    /**
+     * Construct a new game
+     */
+    public VoyageQuest() {
+       super("We are Team Coding Voyage!");
+    }
+
+    /**
+     * Initialize the game container and start the scripting engine
+     * @param gc the new game container
+     * @throws SlickException something went wrong with the Slick engine
+     */
+    @Override
+    public void init(GameContainer gc) throws SlickException {
+        
+    }
+
+
+    /**
+     * Update the screen
+     * @param gc the game container
+     * @param delta time interval
+     * @throws SlickException something went horribly wrong with Slick
+     */
+    @Override
+    public void update(GameContainer gc, int delta) throws SlickException {
+        
+        
+    }
+
+    /**
+     * Render the game window
+     * @param gc the game container
+     * @param g the graphics engine
+     * @throws SlickException something went horribly wrong with Slick
+     */
+    @Override
+    public void render(GameContainer gc, Graphics g) throws SlickException {
+          
+    }
+
+    /**
+     * The main method<br/>
+     * Create the game window and start the game!
+     * @param args command line arguments
+     * @throws SlickException something went horribly wrong with Slick
+     */
+    public static void main(String[] args) throws SlickException {
+        
+        // Start new game window
+        AppGameContainer app = new AppGameContainer(new VoyageQuest());
+
+        app.setDisplayMode(X_RESOLUTION, Y_RESOLUTION, FULLSCREEN);
+        app.setAlwaysRender(true);
+        app.setTargetFrameRate(60);
+
+        app.start();
+
+    }
+}
